dabd5274f8cbbd8c74c02faac954e0f973b30932
Brad Smith <brad.smith@exactearth.com>
2014-03-28 20:11:41 -0400

    Wrote some code with Ghost.py, but can't seem to make it work. Checking-in here for reference, but I'm probably going to switch to mechanize.

--- check_for_jobs.py
+++ check_for_jobs.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python
 from ghost import Ghost
 
+USERNAME = ""
+PASSWORD = ""
+SITE_URL = ""
+
 
 def main():
     ghost = Ghost()
+
+    page, extra_resources = ghost.open(SITE_URL)
+    assert page.http_status == 200
+
+    result, resources = ghost.set_field_value("input[name=UserName]", USERNAME)
+    result, resources = ghost.set_field_value("input[name=Password]", PASSWORD)
+
+    ghost.click("input[name=Submit]")
+    page, extra_resources = ghost.wait_for_page_loaded()
+    #assert page.http_status == 200
+
+    print page.content
 
 if __name__ == "__main__":
     main()
