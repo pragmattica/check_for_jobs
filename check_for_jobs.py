a650cb4f508c30f16cf9a39c7c6a1154d5c3d8e2
Brad Smith <brad.smith@exactearth.com>
2014-09-28 17:52:58 -0400

    Implemented parsing for the awful date format used by the web-site. Can now calculate the duration of each job.

--- check_for_jobs.py
+++ check_for_jobs.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
-import os, mechanize, datetime, time, subprocess
+import os, mechanize, datetime, time, subprocess, pytz
+from pytz import timezone
 
 from bs4 import BeautifulSoup
 
@@ -9,11 +10,45 @@ SITE_URL = ""
 TO_NOTIFY = [""]
 
 
+class Job(object):
+    def __init__(self, requested, job_id_str, desc_str):
+        self.requested = requested
+        self.job_id_str = job_id_str
+        self.desc_str = desc_str
+        self.start_datetime, self.end_datetime = self.parse_desc_str(self.desc_str)
+        self.duration = self.end_datetime - self.start_datetime
+        self.num_hours = self.duration.total_seconds() // 3600
+        self.num_minutes = (self.duration.total_seconds() - (self.num_hours * 3600)) // 60
+        self.duration_str = str(int(self.num_hours)).zfill(2) + ":" + str(int(self.num_minutes)).zfill(2)
+
+    def parse_special_date_time(self, date_str, time_str):
+        month_str, day_str, year_str = date_str.split('/')
+        hour_str, minute_str = time_str.split(':')
+        revised_datetime_str = month_str.zfill(2) + '/' + day_str.zfill(2) + '/' + year_str + ' ' + hour_str.zfill(2) + ":" + minute_str
+        if revised_datetime_str.endswith('AM'): revised_datetime_str = revised_datetime_str[:-2] + 'am'
+        if revised_datetime_str.endswith('PM'): revised_datetime_str = revised_datetime_str[:-2] + 'pm'
+        naive_datetime_obj = datetime.datetime.strptime(revised_datetime_str, "%m/%d/%Y %I:%M%p")
+        eastern = timezone('America/Toronto')
+        datetime_obj = eastern.localize(naive_datetime_obj)
+        return datetime_obj
+
+    def parse_desc_str(self, desc_str):
+        cut_str = desc_str.strip()
+        if desc_str.endswith(" (A) ") or desc_str.endswith(" (S)"):
+            cut_str = cut_str[:-4]
+            cut_str = cut_str.strip()
+        start_datetime_str, end_datetime_str = cut_str.split(" until ")
+        start_date_str, start_time_str = start_datetime_str.split(" at ")
+        end_date_str, end_time_str = end_datetime_str.split(" at ")
+        start_date_obj = self.parse_special_date_time(start_date_str, start_time_str)
+        end_date_obj = self.parse_special_date_time(end_date_str, end_time_str)
+        return (start_date_obj, end_date_obj)
+
 # Parses the jobs list and returns a tuple containing a list of requested jobs and a second list
 # of available jobs.
 def parse_jobs(avail_response_html):
     # Figure out if the table of jobs is split into two sections
-    soup = BeautifulSoup(avail_response_html)
+    soup = BeautifulSoup(avail_response_html, "lxml")
     tables = soup.find_all('table')
     for t in tables:
         data = t.find_all('td')
@@ -24,17 +59,20 @@ def parse_jobs(avail_response_html):
                 request_list, avail_list = [], []
                 for r in rows[2:]:
                     if in_request_section == True and len(r.find_all('td')) == 5:
-                        request_list.append(r.find_all('td')[4])
+                        new_req_job = Job(True, 0, r.find_all('td')[4].text)
+                        request_list.append(new_req_job)
                     elif in_request_section == True and len(r.find_all('td')) != 5:
                         in_request_section = False
                     elif in_request_section == False and len(r.find_all('td')) == 5:
-                        avail_list.append(r.find_all('td')[4])
+                        new_avail_job = Job(False, 0, r.find_all('td')[4].text)
+                        avail_list.append(new_avail_job)
                 return (request_list, avail_list)
 
             else: # Single section
                 avail_list = []
                 for r in rows[1:]:
-                    avail_list.append(r.find_all('td')[4])
+                    new_avail_job = Job(False, 0, r.find_all('td')[4].text)
+                    avail_list.append(new_avail_job)
                 return ([], avail_list)
 
 def notify_all(msg, title):
